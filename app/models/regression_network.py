5753eeeb2e9a9c4a371b1b6859cf9a77381bc7ed
Arturo Negreiros <anegreiross@outlook.com>
2021-07-26 15:02:00 -0500

    defining process

---
+++ app/models/regression_network.py
@@ -0,0 +1,26 @@
+
+import pandas as pd
+from tensorflow.keras import (
+    models,
+    layers,
+    optimizers
+)
+
+
+class NeuronNetworkRegression:
+
+    def __init__(self, learning_rate: float, input_shape_val: int, path: str) -> None:
+        self.learning_rate = learning_rate
+        self.input_shape_val = input_shape_val
+        self.dataset = pd.read_csv(path)
+
+    def _define_model(self):
+        model = models.Sequential()
+        model.add(layers.Dense(64, activation="relu", input_shape=self.input_shape_val, ))
+        model.add(layers.Dense(64, activation="relu"))
+        model.add(layers.Dense(1))
+
+        return model
+
+    def init_trainings(self):
+        model = self._define_model()
--- main.py
+++ main.py
@@ -2,9 +2,8 @@
 
 import pandas as pd
 import numpy as np
-from tensorflow import keras
 
-from keras import (
+from tensorflow.keras import (
     layers,
     models,
     optimizers
@@ -13,44 +12,22 @@ from keras import (
 from sklearn.model_selection import train_test_split
 from utils.utils import Utils
 
-class NeuronNetworkRegression:
-
-    def __init__(self, learning_rate: float, input_shape_val: int) -> None:
-        self.learning_rate = learning_rate
-        self.input_shape_val = input_shape_val
-
-    def _define_model(self):
-        model = models.Sequential()
-        model.add(layers.Dense(64, activation="relu", input_shape=self.input_shape_val,))
-        model.add(layers.Dense(64, activation="relu"))
-        model.add(layers.Dense(1))
-
-        return model
-
-    def init_trainings(self):
-        model = self._define_model()
-
-
-
-
 
 def main():
     # utilities = Utils()
     # print(utilities.load_from_csv("data/yogurt_dataset_nn.csv"))
-    
-    datamaster = pd.read_csv("data/beta_dataset.csv")
+
+    data_master = pd.read_csv("data/beta_dataset.csv")
     """
     Index(['streptococcus_initial_strain_cfu_ml',
        'lactobacillus_initial_strain_cfu_ml', 'ideal_temperature_c',
        'minimum_milk_proteins', 'titratable_acidity', 'pH_milk_sour_',
        'fat_milk_over_100mg_', 'quality_product'],
     """
-    y_strep = datamaster["streptococcus_initial_strain_cfu_ml"]
-    y_lact =  datamaster["lactobacillus_initial_strain_cfu_ml"]
+    y_strep = data_master["streptococcus_initial_strain_cfu_ml"]
+    y_lact = data_master["lactobacillus_initial_strain_cfu_ml"]
 
-    X = datamaster.drop(["streptococcus_initial_strain_cfu_ml", "lactobacillus_initial_strain_cfu_ml"], axis = 1)
-    print(datamaster.columns)
-
+    X = data_master.drop(["streptococcus_initial_strain_cfu_ml", "lactobacillus_initial_strain_cfu_ml"], axis=1)
 
 
 if __name__ == "__main__":
