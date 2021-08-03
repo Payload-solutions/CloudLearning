3a5bf66676b683898dff6b14afebc173108f9f6e
Arturo0911 <anegreiross@outlook.com>
2021-08-03 01:04:31 -0500

    building front end

--- cloud-client/src/components/Header.jsx
+++ cloud-client/src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import '../assets/styles/components/Header.scss'
-
+import logo from '../assets/static/deep.png'
 // Presentational components, not logic
 // only presentation in html
 
 const Header = () => (
     <header className="header">
-        <img className="header__img" src="../assets/images/deeplearning_icon.png" alt="neuron network icon" />
+        <img className="header__img" src={logo} alt="neuron network icon" />
         <div className="header__menu">
             <div className="header__menu--profile">
                 <img src="" alt="" />
--- cloud-client/src/containers/App.jsx
+++ cloud-client/src/containers/App.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import Header from '../components/Header';
-import Search from '../components/Search'
+import Search from '../components/Search';
+import Footer from '../components/Footer'
+// import Catgories from '../components/Categories';
+// import Carousel from '../components/Carousel';
+// import CarouselItem from '../components/CarouselItem';
+// import Categories from '../components/Categories';
 import '../assets/styles/App.scss';
+
 
 const App = () => (
     <div className="App">
         <Header/>
         <Search/>
+        {/* <Categories>
+            <Carousel>
+                <CarouselItem/>
+                <CarouselItem/>
+                <CarouselItem/>
+                <CarouselItem/>
+            </Carousel>
+        </Categories> */}
+        <Footer/>
     </div>
 );
 
