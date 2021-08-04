333aaa54bc615f20eb40d69df9a628b47df2e354
Arturo0911 <anegreiross@outlook.com>
2021-08-04 02:34:26 -0500

    building front end

--- cloud-client/src/containers/Tables.jsx
+++ cloud-client/src/containers/Tables.jsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-
+import React, { useEffect, useState } from 'react';
+import "../assets/styles/components/Tables.scss"
 /**
  *@author Arturo Negreiros
  * this sections will contain the values extracted from the database, that is, cassandra sql
@@ -9,14 +9,31 @@ import React, {useEffect, useState} from 'react';
 const Tables = () => {
     const [numbers, Setnumbers] = useState([]);
     const APIFlsk = "http://192.168.100.8:5000/test";
-    useEffect(() =>{
+    useEffect(() => {
         fetch(APIFlsk)
             .then(response => response.json())
             .then(data => Setnumbers(data))
     }, []);
     console.log(numbers)
     return (
-        <div></div>
+        <div className="table_content">
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">pH</th>
+                        <th scope="col">Acidez</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
     );
 }
 
