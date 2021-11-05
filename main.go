16603237f3af7d3fbc9209611cc9a4332d5f9927
Arturo0911 <anegreiross@outlook.com>
2021-11-04 21:23:51 -0500

    Compiled successfully golearn

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/kniren/gota/dataframe"
+)
+
+func getXYMat() { //(*mat.Dense, *mat.Dense) {
+	file, err := os.Open("neural_model/data/data_regression_set.csv")
+	if err != nil {
+		//return nil, nil
+		return
+	}
+	defer file.Close()
+	df := dataframe.ReadCSV(file)
+	yData := df.Col("quality_product")
+	fmt.Println(yData)
+
+}
+
+func main() {
+	getXYMat()
 }
