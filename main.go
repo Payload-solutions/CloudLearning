b7741c8f9e4ca6efa1cf226bea810fd54f02a949
Arturo0911 <anegreiross@outlook.com>
2021-11-04 22:26:04 -0500

    ok

--- main.go
+++ main.go
@@ -2,24 +2,32 @@ package main
 
 import (
 	"fmt"
-	"os"
 
-	"github.com/kniren/gota/dataframe"
+	"github.com/sjwhitworth/golearn/base"
 )
 
-func getXYMat() { //(*mat.Dense, *mat.Dense) {
-	file, err := os.Open("neural_model/data/data_regression_set.csv")
-	if err != nil {
-		//return nil, nil
-		return
-	}
-	defer file.Close()
-	df := dataframe.ReadCSV(file)
-	yData := df.Col("quality_product")
-	fmt.Println(yData)
+//"github.com/sjwhitworth/golearn/base"
 
+// func getXYMat() { //(*mat.Dense, *mat.Dense) {
+// 	file, err := os.Open("neural_model/data/data_regression_set.csv")
+// 	if err != nil {
+// 		//return nil, nil
+// 		return
+// 	}
+// 	defer file.Close()
+// 	df := dataframe.ReadCSV(file)
+// 	yData := df.Col("quality_product")
+// 	fmt.Println(yData)
+// }
+
+func yogurtPrediction() {
+	rawData, err := base.ParseCSVToInstances("neural_model/data/data_regression_set.csv", true)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(rawData)
 }
 
 func main() {
-	getXYMat()
+	yogurtPrediction()
 }
