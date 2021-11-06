1fc52baa528ceef73bb935a7a264489692757e5a
Arturo0911 <anegreiross@outlook.com>
2021-11-05 23:06:47 -0500

    ok

--- lacto/lacto.go
+++ lacto/lacto.go
@@ -5,17 +5,17 @@ import (
 	"os"
 
 	"github.com/kniren/gota/dataframe"
+	"gonum.org/v1/gonum/mat"
 )
 
-func GetXYMat() { //(*mat.Dense, *mat.Dense) {
+func GetXYMat() (*mat.Dense, *mat.Dense) {
 	file, err := os.Open("neural_model/data/data_regression_set.csv")
 	if err != nil {
-		return
-
+		return nil, nil
 	}
 	defer file.Close()
 	df := dataframe.ReadCSV(file)
-	//yData := df.Col("lactobacillus_initial_strain_cfu_ml")
+	yData := df.Col("lactobacillus_initial_strain_cfu_ml")
 	xData := df.Drop([]string{"streptococcus_initial_strain_cfu_ml",
 		"lactobacillus_initial_strain_cfu_ml", "quality_product", "lactobacillus_final_cfu_ml", "streptococcus_final_cfu_ml"})
 	//fmt.Println(yData)
